32ff231feaf78f5c333c50afb73a91e438c119c6
XCVGVCX <xcvgvcx@gmail.com>
2020-05-20 12:23:57 -0700

    more argument parsing, exception handling and error code return

--- Bigly/Program.cs
+++ Bigly/Program.cs
@@ -5,34 +5,48 @@ namespace Bigly
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             try
             {
+                if (args.Length == 0)
+                    throw new NotSupportedException($"An operation must be specified");
+
                 //TODO args
                 //TODO logging
 
-                if (!args[0].Equals("extract", StringComparison.OrdinalIgnoreCase))
+                if (args[0].Equals("extract", StringComparison.OrdinalIgnoreCase))
+                {
+                    string filePath = args[1];
+                    string fileName = Path.GetFileName(filePath);
+
+                    Console.WriteLine("Extracting " + fileName);
+
+                    byte[] data = File.ReadAllBytes(filePath);
+                    //GlobalHeader globalHeader = GlobalHeader.FromBytes(data);
+                    BigArchive big = BigArchive.FromBytes(data);
+
+                    string outputPath = Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileNameWithoutExtension(fileName));
+                    big.WriteAllContents(outputPath);
+
+                    Console.WriteLine("Done extracting " + fileName);
+                }
+                else if (args[0].Equals("pack", StringComparison.OrdinalIgnoreCase))
+                {
                     throw new NotImplementedException();
+                }
+                else
+                {
+                    throw new NotSupportedException($"Operation \"{args[0]}\" is not supported");
+                }
 
-                string filePath = args[1];
-                string fileName = Path.GetFileName(filePath);
-
-                Console.WriteLine("Extracting " + fileName);
-
-                byte[] data = File.ReadAllBytes(filePath);
-                //GlobalHeader globalHeader = GlobalHeader.FromBytes(data);
-                BigArchive big = BigArchive.FromBytes(data);
-
-                string outputPath = Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileNameWithoutExtension(fileName));
-                big.WriteAllContents(outputPath);
-
-                Console.WriteLine("Done extracting " + fileName);
+                return 0;
             }
             catch(Exception e)
             {
                 Console.Error.WriteLine($"Fatal error: {e.GetType().Name}");
                 Console.Error.WriteLine(e.Message);
+                return 1;
             }
         }
     }
